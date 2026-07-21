647e0ee90ae7c4801bcfd22efe2352abc46e83d5
raseleditz <raselffbusiness@gmail.com>
2026-07-21 15:59:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Telegram Verification Server is Running!');
+});
+
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHANNEL_USERNAME = '@raselff148';
 
